200a8f0265065f494dde9ff5b3ca8fdb42fdbeee
minju0307 <86367674+minju0307@users.noreply.github.com>
2023-10-27 10:24:05 +0000

    sys로 입력받아야 시간초과가 안 난다

--- 삼각형_만들기.py
+++ 삼각형_만들기.py
@@ -1,11 +1,10 @@
-N = int(input())  ## N의 개수가 벡민이기 떄문에 linear time 으로 짜야함
-length = []
-for i in range(N):
-  length.append(int(input()))
-length = sorted(length, reverse=True)
+import sys
+
+N = int(sys.stdin.readline())  ## N의 개수가 벡민이기 떄문에 linear time 으로 짜야함
+length = sorted([int(sys.stdin.readline()) for _ in range(N)], reverse=True)
 answer = -1
 
-for i in range(N-1):
+for i in range(N-2):
   if length[i] < length[i+1] + length[i+2]:
     answer = length[i] + length[i+1] + length[i+2]
     break
