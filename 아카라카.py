e95f3b2d796862b730cb08b092e0d0261d422d6d
minju0307 <86367674+minju0307@users.noreply.github.com>
2023-11-14 11:45:54 +0000

    재귀적으로 접두사 접미사도 팰린드롬인지 확인해야 함

--- 아카라카.py
+++ 아카라카.py
@@ -2,14 +2,29 @@ import sys
 
 S = sys.stdin.readline().strip()
 
-if len(S) == 1:
-  print('AKARAKA')
+## 아카라카 팰린드롬인지 아닌지 확인하는 함수 
+## 재귀적으로 들어가서 prefix와 suffix가 각각 아카라카 팰린드롬인지 확인하기 
 
-else:
+def check (S):
+  ## S의 길이가 1이면 아카라카 팰린드롬
+  if len(S) == 1:
+    return True
+  
   prefix = S[:len(S) // 2]
   suffix = S[len(S) // 2:] if len(S) % 2 == 0 else S[len(S) // 2 + 1:]
-
-  if S == S[::-1] and prefix == prefix [::-1] and suffix == suffix [::-1] :
-    print('AKARAKA')
+  
+  ## 만약에 아카라카 팰린드롬이라면, prefix와 suffix도 팰린드롬인지 확인하기 
+  if S == S[::-1] and prefix == prefix[::-1] and suffix == suffix[::-1]:
+    return check(prefix) and check(suffix)
+  ## 만약에 아카라카 팰린드롬이 아니면, 바로 False를 출력하기 
   else:
-    print('IPSELENTI')
+    return False
+
+
+if check(S):
+  print('AKARAKA')
+else:
+  print('IPSELENTI')
+
+
+
