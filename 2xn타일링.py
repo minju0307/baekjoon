5f3c2b15267db3cc4ecc6d60ad6b578058298b25
minju0307 <86367674+minju0307@users.noreply.github.com>
2023-11-14 13:19:06 +0000

    index 에러 고려해서 처음 리스트를 입력될 수 있는 만큼 설정

--- 2xn타일링.py
+++ 2xn타일링.py
@@ -2,12 +2,12 @@ import sys
 
 n = int(sys.stdin.readline().strip())
 
-array = [0]*n 
-array[0] = 1
-array[1] = 2
+array = [0]*1001 
+array[1] = 1
+array[2] = 2
 
-for i in range(2, n):
+for i in range(3, n+1):
   array[i] = array[i-1] + array[i-2]
 
-print(array[-1]%10007)
+print(array[n]%10007)
   
