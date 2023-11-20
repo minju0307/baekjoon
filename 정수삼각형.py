19c41dd2f813045a46759a2dfa03ce907b34b052
minju0307 <86367674+minju0307@users.noreply.github.com>
2023-11-20 10:23:29 +0000

    업데이트 오류 방지때문에 커밋

--- 정수삼각형.py
+++ 정수삼각형.py
@@ -7,11 +7,17 @@ for i in range(n):
   triangles.append(list(map(int,sys.stdin.readline().strip().split())))
 
 # print(triangles)
-dp = [0]*500
+dp = [0] * 501
+dp[1] = triangles[1][0]
 
-for i in range(n, 0, -1):
-  triangles[i].insert(0,0)
-  for j in range(n-i):
+for i in range(2, n + 1):
+
+  ## 계산하기 편하도록 리스트를 조정하기
+  triangles[i].insert(0, 0)
+  for j in range(n - i):
     triangles[i].append(0)
 
+  ## 현재 level에서 모든 경우의 수를 합한 것을 저장하여 최상의 조합이 무엇인지 찾기
+
 print(triangles)
+print(dp[:n])
