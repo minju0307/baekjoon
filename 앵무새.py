c4767a30201e6ad0a608996c526a8fbed6c4bad8
minju0307 <86367674+minju0307@users.noreply.github.com>
2023-11-23 07:44:32 +0000

    주석 추가

--- 앵무새.py
+++ 앵무새.py
@@ -5,23 +5,25 @@ input = sys.stdin.readline
 
 
 def check(s, l):
-  first = [len(i) for i in s] + [len(l)]
+  first = [len(i) for i in s] + [len(l)]  ## 단어들의 길이를 큐에 넣어준다.
   q = deque([first])
   count = 0
-  while q:  ## len(l) 만큼만 확인하기
+  while q:  ## 큐가 비어 있지 않을 때까지
     idx_list = q.popleft()
 
-    if idx_list[-1] == 0:
+    ## 종료조건
+    if idx_list[-1] == 0:  ## l의 모든 단어들을 순차적으로 찾은 경우에는 true
       return True
-    if count == len(l):
+    if count == len(l):  ## l의 개수만큼 돌았는데, l의 단어들이 사라지지 않은 경우에는 false
       return False
 
-    word = l[idx_list[-1] - 1]
+    word = l[idx_list[-1] - 1]  ## 현재 확인하고자 하는 단어
     # print(word)
     # print(idx_list)
     current = []
     last = idx_list[-1]
     for s_idx, w_idx in enumerate(idx_list[:-1]):
+      ## 만약 현재 단어가 특정 앵무새의 마지막 말 단어와 같다면 인덱스를 하나 앞으로 빼준다.
       if w_idx - 1 < len(s[s_idx]) and word == s[s_idx][w_idx - 1]:
         current.append(w_idx - 1)
         last = idx_list[-1] - 1
