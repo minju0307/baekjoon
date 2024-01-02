026f521cf87e61231c62d0153faaee5c7360a6c5
minju0307 <86367674+minju0307@users.noreply.github.com>
2024-01-02 12:56:22 +0000

    앞 숫자부터 차례대로 비교하게끔, 60퍼센트 정답

--- 가장큰수.py
+++ 가장큰수.py
@@ -1,22 +1,18 @@
-## 조건들을 적어보기
-## 하나씩 자릿수를 내려가면서 정렬하기 그러니까, 앞 자리수가 큰 것 순서대로 계속 정렬이 되어 있어야 하고, 그것은 뒷 자리수 비교할 때 영향을 끼치면 안됨!
-## 길이가 긴 것들이 뒤로 가야함
-
-
 def solution(numbers):
-  i = 0
   numbers = list(map(str, numbers))
 
   def function(x):
-    if i < len(x):
-      return (x[i], len(x))
-    else:
-      return (x[-1], len(x))
+    if len(x) == 4:
+      return (x[0], x[1], x[2], x[3])
+    elif len(x) == 3:
+      return (x[0], x[1], x[2], x[2])
+    elif len(x) == 2:
+      return (x[0], x[1], x[1], x[1])
+    elif len(x) == 1:
+      return (x[0], x[0], x[0], x[0])
 
-  for k in range(4):
-    numbers = sorted(numbers, key=function, reverse=True)
-    print(numbers)
-    i += 1
+  numbers = sorted(numbers, key=function, reverse=True)
+  # print(numbers)
 
   answer = ''.join(numbers)
   return str(int(answer))
