b8bd2abb7ce9fdc70112f6ec48f47fc98873a990
minju0307 <86367674+minju0307@users.noreply.github.com>
2024-01-02 09:58:36 +0000

    sorted 함수를 선언해서 사용

--- 가장큰수.py
+++ 가장큰수.py
@@ -1,14 +1,23 @@
 def solution(numbers):
-
+  i = 0
   numbers = list(map(str, numbers))
-  numbers = sorted(numbers)
-  print(numbers)
+
+  # numbers = sorted(numbers, reverse=True)
+  # print(numbers)
+
+  def function(x):
+    if i < len(x):
+      return x[i]
+    else:
+      return x[-1]
+
   for i in range(4):
-    numbers = sorted(numbers, key=lambda x: x[-1], reverse=True)
+    numbers = sorted(numbers, key=function, reverse=True)
     print(numbers)
+    i += 1
 
   answer = ''.join(numbers)
-  return answer
+  return str(int(answer))
 
 
 if __name__ == '__main__':
