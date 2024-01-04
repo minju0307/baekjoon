5d9330bbaea6fc43c26571180f18335fbfb673b7
minju0307 <86367674+minju0307@users.noreply.github.com>
2024-01-04 01:47:42 +0000

    deque 사용

--- 같은숫자는싫어.py
+++ 같은숫자는싫어.py
@@ -1,6 +1,18 @@
+from collections import deque
+
+
 def solution(arr):
-  answer = list(set(arr))
+  answer = []
+  dq = deque()
+  dq.append(arr[0])
+
+  for i in range(1, len(arr)):
+    current = dq[-1]
+    if current != arr[i]:
+      dq.append(arr[i])
+
   return answer
 
+
 if __name__ == "__main__":
-  print(solution([1,1,3,3,0,1,1]))
+  print(solution([1, 1, 3, 3, 0, 1, 1]))
