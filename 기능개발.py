5ba4caaa50a17f2648d93318f53b7a26060cf79c
minju0307 <86367674+minju0307@users.noreply.github.com>
2024-01-04 03:55:25 +0000

    dq에 progresses와 speed를 같이 넣기, 마지막 한 개 남은 경우를 처리하기

--- 기능개발.py
+++ 기능개발.py
@@ -3,28 +3,38 @@ from collections import deque
 
 def solution(progresses, speeds):
   answer = []
-  dq = deque(progresses)
-  print(dq)
+  dq = deque()
+  for idx, w in enumerate(progresses):
+    dq.append([w, speeds[idx]])
+
+  # print(dq)
+
   while dq:
-    print("***")
+    # print("***")
+
     ## 하루치 개발 진도량을 반영하기
-    for idx, work in enumerate(progresses):
-      dq[idx] = dq[idx]+ speeds[idx]
-    print(dq)
+    for idx, ll in enumerate(list(dq)):
+      dq[idx][0] = ll[0] + ll[1]
+    # print(dq)
 
     ## popleft 하면서 100이 넘는 게 있다면 빼버리기
+
     deploy = 0  ## 오늘 배포 가능한 것의 숫자
     current = dq.popleft()
-    while current >= 100:
+    while dq and current[0] >= 100:
       deploy += 1
       current = dq.popleft()
-    if current < 100:
+
+    if current[0] < 100:
       dq.appendleft(current)
-    print(dq)
+    else:
+      deploy += 1
+
+    # print(dq)
 
     if deploy > 0:
       answer.append(deploy)
-    print(deploy)
+    # print(deploy)
 
   return answer
 
