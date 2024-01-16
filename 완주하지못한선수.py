ba9d1e436056d7268fb05bfa1769f94596382d2b
mjk0307 <0307mjk@naver.com>
2024-01-16 19:12:47 +0900

    시간 초과 2 번째

--- 완주하지못한선수.py
+++ 완주하지못한선수.py
@@ -2,14 +2,8 @@
 
 def solution(participant, completion):
     
-    score = [0 for _ in range(len(participant))]
-    
-    for idx, name in enumerate(participant):
+    for name in participant:
         if name in completion:
-            score[idx] += 1
             completion.remove(name)
-    
-    answer = participant[score.index(0)]
-    
-    
-    return answer
+        else:
+            return name
