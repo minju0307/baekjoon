768b8a4613102e05f64202268b34117c4a312a14
mjk0307 <0307mjk@naver.com>
2024-01-18 13:02:22 +0900

    dfs로 풀기

--- 타겟넘버.py
+++ 타겟넘버.py
@@ -1,23 +1,18 @@
-from itertools import combinations
-
 def solution(numbers, target):
-    answer = 0
     
-    pos = [i for i in range(len(numbers))]
+    def check(idx, local_hap):
+        nonlocal answer
     
-    for i in range(1, len(pos)+1):
-        pos_idx = list(combinations(pos, i))
-        
-        ## 개수별 조합에 대하여  
-        for j in pos_idx:
-            hap = 0
-            for k in range(len(numbers)):
-                if k in j:
-                    hap += numbers[k]
-                else:
-                    hap -= numbers[k]
-            if hap == target:
-                answer +=1  
-        
+        if idx == len(numbers):
+            if local_hap == target:
+                answer += 1
+            return 
     
+        check(idx+1, local_hap+numbers[idx]) ## 더해주는 경우
+        check(idx+1, local_hap-numbers[idx]) ## 빼주는 경우 
+    
+    
+    answer = 0 
+    check(0, 0)
+
     return answer
