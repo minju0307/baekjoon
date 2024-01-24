3555c2867808c5d6bfbcb0217619aa9542b0031a
mjk0307 <0307mjk@naver.com>
2024-01-24 20:54:34 +0900

    네트워크 최종정답

--- 네트워크.py
+++ 네트워크.py
@@ -1,39 +1,36 @@
 def find_parent(parent, x):
     if parent[x] != x:
-        return find_parent(parent, parent[x])
-    return x
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
-def union (parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-    if a < b:
-        parent[b] = a
+def union (parent, i, j):
+    ir = find_parent(parent, i)
+    jr = find_parent(parent, j)
+    if ir < jr:
+        parent[jr] = ir
     else:
-        parent[a] = b
+        parent[ir] = jr
 
 def solution(n, computers):
     
     ## 부모 테이블을 자기 자신으로 초기화 
     parent = [i for i in range(n)]
-    print(parent)
     
     ## edge가 연결되어 있으면 union 연산을 수행 
     for i, computer in enumerate(computers):
-        # print()
-        # print(f"***{i}***")
         for j, edge in enumerate(computer):
-            # print(j)
             if i==j:
                 continue
-            elif edge == 1 and (parent[i] != parent[j]):
+            elif edge == 1:
                 union(parent, i, j)
     
-    print(parent)
+    ## 부모 행렬의 root를 찾아내서 정리하는 것이 필요함. 
     for i in range(n):
         parent[i] = find_parent(parent, i)
+    
     answer = len(list(set(parent)))
     
     return answer
 
 if __name__ == '__main__':
-    print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
+    print(solution(4, [[1, 0, 0, 1], [0, 1, 1, 0], [0, 1, 1, 1], [1, 0, 1, 1]]))
