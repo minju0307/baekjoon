e7a7fc45a6a203952f489093e216fdfcabf7f6bb
mjk0307 <0307mjk@naver.com>
2024-01-29 15:25:11 +0900

    bfs 큐에 최단 경로 함께 저장하면서 -> 1번 테케 틀림

--- 단어변환.py
+++ 단어변환.py
@@ -1,32 +1,26 @@
-
 from collections import deque 
             
 def solution(begin, target, words):
     
-    words.append(target)
-    words = list(set(words))
-    words = [(idx+1, w) for idx, w in enumerate(words)]
+    if target not in words:
+        return 0
     
-    print(words)
-    print(begin)
-    print(target)
+    # print(words)
+    # print(begin)
+    # print(target)
     
-    visited = [0]*(len(words)+1)
-    
-    queue = deque([(0, begin)])
-    visited[0] = 1
-    distance = 0
-    answer = 0
+    visited = [0]*(len(words))
+    queue = deque([(begin, 0)]) ## bfs 시작, 단어와 현재까지의 경로를 큐에 같이 넣어주기 
     
     while queue:
-        print()
-        print(queue)
+        # print()
+        # print(queue)
         current = queue.popleft()
         
         if current == target:
             break
         
-        for w_idx, w in words:
+        for w_idx, w in enumerate(words):
             ## 이미 방문 했던 단어는 넘어가기 / 자기 자신도 넘어가게 됨 
             if visited[w_idx] == 1 :
                 continue
@@ -35,17 +29,16 @@ def solution(begin, target, words):
             correct = 0
             wrong = 0
             for iidx, i in enumerate(list(w)):
-                if i == current[1][iidx]:
+                if i == current[0][iidx]:
                     correct += 1
                 else:
                     wrong += 1
             
-            if correct == len(current[1])-1 and wrong == 1:
-                queue.append((w_idx, w))
+            if correct == len(current[0])-1 and wrong == 1:
+                queue.append((w, current[1]+1))
                 visited[w_idx] = 1
     
-
-    return answer
+    return current[1]
 
 if __name__ == "__main__":
     solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"])
