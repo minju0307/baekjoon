714a1f6aa720712455ed297594a294e3e8566372
mjk0307 <0307mjk@naver.com>
2024-01-29 17:20:11 +0900

    한 개짜리는 빼면서 효율적으로 구성하였지만 여전히 하나의 테케에서 실패

--- 의상.py
+++ 의상.py
@@ -2,28 +2,23 @@ from collections import defaultdict
 from itertools import combinations
 
 def solution(clothes):
-    answer = 0
     
     dic = defaultdict(int)
-    for n, t in clothes:
+    for _, t in clothes:
         dic[t] += 1
         
+    answer = sum(list(dic.values()))
     types = list(dic.keys())
     
-    for i in range(1, len(types)+1):
+    for i in range(2, len(types)+1):
         comb = combinations(types, i) 
 
         for tup in list(comb):
-            # print(tup)
-            if i == 1:
-                answer += dic[tup[0]]
-            else:
-                tmp = 1
-                for sub in tup:
-                    tmp *= dic[sub]
-                answer += tmp 
-            # print(answer)
-
+            tmp = 1
+            for sub in tup:
+                tmp *= dic[sub]
+            answer += tmp 
+            
     return answer
 
 if __name__=='__main__':
