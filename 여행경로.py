2d87c182047e2c9699c6c7149273ed3a5d560a36
mjk0307 <0307mjk@naver.com>
2024-02-01 23:40:07 +0900

    여행 경로 스택으로 해결!

--- 여행경로.py
+++ 여행경로.py
@@ -1,36 +1,29 @@
-
-def dfs(d, a, idx, visited, tickets, answer):
-    
-    ## 종료 조건 
-    if 0 not in visited:
-        answer.append(a)
-        if (len(answer) == len(tickets)+1): ## 모든 티켓이 사용되었고, 모든 공항을 방문했다면 
-            ans.append(answer)
-            return
-    
-    ## 다음 지점 후보 선택
-    for new_idx, (new_d, new_a) in enumerate(tickets): 
-        if visited[new_idx] == 1: continue
-        if a == new_d:
-            answer.append(new_d)
-            visited[new_idx]=1
-            dfs(new_d, new_a, new_idx, visited, tickets, answer)
-            visited[new_idx]=0
-            answer=answer[:-1]
-
 def solution(tickets):
-    global ans
+    routes = {} ## key: 시작지점, value: 도착 지점 / 갈 수 있는 공항이 있는지 없는지 확인하기 위하여 / 중복 경로도 구분이 됨 
+    for t in tickets:
+        routes[t[0]] = routes.get(t[0], []) + [t[1]]
+        
+    for r in routes:
+        routes[r].sort(reverse=True) ## 도착 지점들은 알파벳 거꾸로 순서로 정렬 
+        
+    stack = ["ICN"]
+    path = []
     
-    ans = []
-    for idx, (d, a) in enumerate(tickets): ## 시작 지점 후보 선택
-        if d == "ICN":
-            answer = []
-            answer.append("ICN")
-            visited = [0] * len(tickets) ## 항공권을 사용했는지 확인
-            visited[idx] = 1
-            dfs(tickets[idx][0], tickets[idx][1], idx, visited, tickets, answer)
-            visited[idx]=0
-    return min(ans)
+    while len(stack) > 0:
+        print("***")
+        print("routes:", routes)
+        print("stack:", stack)
+        print("path:", path)
+        print()
+        top = stack[-1]
+        
+        if top not in routes or len(routes[top]) == 0: ## 모든 루트를 잘 따라 갔을 때  / 혹은 길을 찾던 도중 더이상 갈 수 있는 공항이 없을 때 
+            path.append(stack.pop()) ## stack에서 지우기 / path에 경로를 추가하기 
+        else:
+            stack.append(routes[top][-1]) ## 다음에 갈 수 있는 공항 중 알파벳이 빠른 순서를 stack에 넣기 
+            routes[top] = routes[top][:-1] ## stack에 넣은 공항은 routes에서 제거하기 
+    
+    return path[::-1]
 
 if __name__=="__main__":
-    print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]))
+    print(solution([["ICN", "AAA"], ["ICN", "CCC"], ["CCC", "DDD"], ["AAA", "BBB"], ["AAA", "BBB"], ["DDD", "ICN"], ["BBB", "AAA"]]))
